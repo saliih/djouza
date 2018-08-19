faee277457ea4286344c92de290228e4c15cb9d1
Salah Chtioui <salah.chtioui@gmail.com>
2018-08-19 15:36:47 +0100

    channel

--- src/AppBundle/Admin/ChannelAdmin.php
+++ src/AppBundle/Admin/ChannelAdmin.php
@@ -44,8 +44,42 @@ class ChannelAdmin extends Admin
     public function prePersist($object)
     {
         $url = "https://www.googleapis.com/youtube/v3/channels?id=".$object->getChannelId()."&key=AIzaSyBGseWi-G-NxC1wO0R4UtTEg0HmSPXSJlI&part=contentDetails";
-        $data = file_get_contents($url);
+        $data = $this->CallAPI("GET",$url);
         echo "<pre>";
         print_r($data);exit;
+    }
+    private function CallAPI($method, $url, $data = false)
+    {
+        $curl = curl_init();
+
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // Optional Authentication:
+        //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
     }
 }
